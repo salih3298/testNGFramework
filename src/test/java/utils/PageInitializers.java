779072a4035bd2875d2689964448ad16a689ae84
salih3298 <salih3298@gmail.com>
2023-10-29 18:58:28 +0300

    testNGFramework

--- src/test/java/utils/PageInitializers.java
+++ src/test/java/utils/PageInitializers.java
@@ -1,4 +1,15 @@
 package utils;
 
 public class PageInitializers {
+    public static LoginPage login;
+    public static EmployeeSearchPage employeeSearchPage;
+    public static AddEmployeePage addEmployeePage;
+    public static DashboardPage dash;
+
+    public static void intializePageObjects(){
+        login = new LoginPage();
+        employeeSearchPage = new EmployeeSearchPage();
+        addEmployeePage = new AddEmployeePage();
+        dash = new DashboardPage();
+    }
 }
